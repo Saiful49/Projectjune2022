a40bdd2b450df5dbb33de6f553d61eb9a467aa46
Saiful49 <107101331+Saiful49@users.noreply.github.com>
2022-06-09 16:22:32 +0800

    Update demofile.py

--- demofile.py
+++ demofile.py
@@ -26,10 +26,11 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-iris = datasets.load_iris(https://raw.githubusercontent.com/Saiful49/Projectjune2022/main/IRIS.csv)
-X= iris.data(['sepal_length	','sepal_width','petal_length','petal_length'], axis = 1)
+iris = datasets.load_iris([https://raw.githubusercontent.com/Saiful49/Projectjune2022/main/IRIS.csv])
+X= iris.data(['sepal_length','sepal_width','petal_length','petal_length'], axis = 1)
 X= head()
 Y = iris.target([species])
+Y = head()
 
 clf = RandomForestClassifier()
 clf.fit(X, Y)
