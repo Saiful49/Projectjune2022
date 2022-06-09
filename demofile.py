2ccac59914102e43fd1dff58feb85790453e4786
Saiful49 <107101331+Saiful49@users.noreply.github.com>
2022-06-09 16:43:30 +0800

    Update demofile.py

--- demofile.py
+++ demofile.py
@@ -26,7 +26,7 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-iris = pd.read.csv('https://raw.githubusercontent.com/Saiful49/Projectjune2022/main/IRIS.csv')
+iris = pd.read_csv('https://raw.githubusercontent.com/Saiful49/Projectjune2022/main/IRIS.csv')
 X= iris.data(['sepal_length','sepal_width','petal_length','petal_length'], axis = 1)
 X= head()
 Y = iris.target(['species'])
